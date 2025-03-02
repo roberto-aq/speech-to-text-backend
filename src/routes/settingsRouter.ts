ada3fa336bb357089abd2786cb7d90ea28ca19e9
roberto-aq <baxro2000@gmail.com>
2025-03-02 10:45:31 -0500

    Credo el router para configuraciones y cambiar env más validacion de body

---
+++ src/routes/settingsRouter.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import type { Request, Response } from 'express';
+import 'dotenv/config';
+import { apiKeyStore } from '../config/apiKeyStore';
+import { body } from 'express-validator';
+import { handleInputErrors } from '../middlewares/validation';
+
+const router = Router();
+
+router.get('/api-key', async (req: Request, res: Response) => {
+	res.status(200).json({
+		message: 'API Settings',
+		apiKeyEnv: process.env.ASSEMBLYAI_API_KEY,
+		apiKeyStore: apiKeyStore.getApiKey(),
+	});
+});
+
+router.post(
+	'/update-api-key',
+	body('newApiKey')
+		.isString()
+		.isLength({ min: 1 })
+		.withMessage('❌ Debes proporcionar una nueva API Key'),
+	handleInputErrors,
+	(req: Request, res: Response) => {
+		const { newApiKey } = req.body;
+
+		apiKeyStore.setApiKey(newApiKey);
+		res.status(200).json({
+			message:
+				'✅ API Key actualizada correctamente. Y reemplazada en el .env',
+		});
+	}
+);
+
+export default router;
