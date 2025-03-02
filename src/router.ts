e0bcabe0c99e073e06c03bd67fcdd5d8f15e8989
roberto-aq <baxro2000@gmail.com>
2025-03-02 05:51:45 -0500

    Configuración del multer

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,27 @@
 import { Router } from 'express';
+import type { Request, Response } from 'express';
+import upload from './config/multer';
 
 const router = Router();
 
-router.post('/audio', (req, res) => {
-	res.status(200).json('Hello World');
-});
+router.post(
+	'/audio',
+	upload.single('audio'),
+	(req: Request, res: Response) => {
+		const { file } = req;
+
+		if (!file) {
+			res.status(400).json('No se ha enviado ningún archivo');
+			return;
+		}
+
+		res.status(200).json({
+			message: 'Archivo recibido con éxito',
+			filename: req.file.originalname,
+			mimetype: req.file.mimetype,
+			size: req.file.size,
+		});
+	}
+);
 
 export default router;
